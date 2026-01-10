76355991e4ee88fe776041d17b12c7db6f12c8dc
VOsadchii <909404@gmail.com>
2026-01-10 21:09:29 +0300

    configure sonar and checkstyle

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("java")
+    id("application")
+    id("checkstyle")
+    id "org.sonarqube" version "7.2.2.6593"
+    id("jacoco")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.check {
+    dependsOn(tasks.checkstyleMain, tasks.checkstyleTest)
+}
+
+tasks.build {
+    dependsOn(tasks.check)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // Для генерации отчета о покрытии
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true) // SonarQube требует XML отчет
+        html.required.set(true)
+    }
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+sonar {
+    properties {
+        property "sonar.projectKey", "valentin-osadchii_java-project-72"
+        property "sonar.organization", "valentin-osadchii"
+    }
+}
+
