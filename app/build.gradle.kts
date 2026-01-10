125fa64ed5d398d6cba90a88d4fcaaf0c6dbd6d6
VOsadchii <909404@gmail.com>
2026-01-10 21:51:10 +0300

    set up javalin app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
     id("application")
     id("checkstyle")
     id("org.sonarqube") version "7.2.2.6593"
     id("jacoco")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+
 }
 
 group = "hexlet.code"
@@ -13,9 +18,21 @@ repositories {
     mavenCentral()
 }
 
+val lombokVersion = "1.18.34"
+
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
 
 tasks.check {
@@ -28,6 +45,13 @@ tasks.build {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
     finalizedBy(tasks.jacocoTestReport) // Для генерации отчета о покрытии
 }
 
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,31 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args)  {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+
+
 }
